ec75f368de0083f40a235f8b72584c699677ffad
Mateus Silva Pereira <mateussilvapereira2018@gmail.com>
2023-11-27 12:47:13 -0400

    Responsive end menu sandwich

--- src/components/Headers/index.jsx
+++ src/components/Headers/index.jsx
@@ -1,9 +1,18 @@
 import styles from './Header.module.css'
 import { Link } from 'react-router-dom';
 import { FaCodeBranch } from 'react-icons/fa'
+import { useState } from 'react';
+import { IoHome } from "react-icons/io5";
+import { ImProfile } from "react-icons/im";
+import { FaProjectDiagram } from "react-icons/fa";
+import { IoIosContact } from "react-icons/io";
 
 function Header() {
-    
+
+    const [ showMenu, setShowMenu] = useState(false);
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    }
     return(
         <header className={styles.header}>
 
@@ -12,12 +21,23 @@ function Header() {
            
             </Link>
 
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/sobre">Sobre</Link>
-                <Link to="/projetos">Projetos</Link>
-                <Link to="/contatos">Contatos</Link>
+            <nav className={`${styles.menuSanduich} ${ showMenu ? styles.show : '' }`}
+                onClick={toggleMenu}
+            >
+                <Link to="/"> <IoHome /> Home</Link>
+                <Link to="/sobre"> <ImProfile /> Sobre</Link>
+                <Link to="/projetos"> <FaProjectDiagram /> Projetos</Link>
+                <Link to="/contatos"> <IoIosContact /> Contatos</Link>
             </nav>
+
+            <div className={styles.menuButton}
+                onClick={toggleMenu}
+            >
+
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+            </div>
         </header>
     )
 }
--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -1,16 +1,16 @@
-import Container from './../../components/Container/index';
+
 import styles from './PageNotFound.module.css'
 
 function PageNotFound() {
 
     return (
-            <Container>
+            <section className={styles.page404}>
                 <h2 className={styles.titulo2}>Algo de errado não está certo!</h2>
                 <div className={styles.textos}>
                     <span className={styles.texto_grande}>404</span><br />
                     <strong className={styles.texto_vermelho}>Página Não Localizada!</strong>
                 </div>
-            </Container>
+            </section>
     );
 }
 
